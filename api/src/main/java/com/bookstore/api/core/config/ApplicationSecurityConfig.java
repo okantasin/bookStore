6348890f35520dfb7742e29125c37bc32d3d6593
okan <okan.tasin14@ogr.atauni.edu.tr>
2022-08-14 14:31:14 +0300

    AppicationSecurityConfig was added then 3 roles added.

---
+++ api/src/main/java/com/bookstore/api/core/config/ApplicationSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.bookstore.api.core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration // This is a configuration class
+@EnableWebSecurity
+@RequiredArgsConstructor // This is a constructor dependency injection
+public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder  passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/books/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/api/v1/users/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails editor = User.builder()
+                .username("editor")
+                .password(passwordEncoder.encode("editor"))
+                .roles("EDITOR")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(admin, editor, user);
+
+
+    }
+}
